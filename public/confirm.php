9a9a523d28849fd4c73c3f2302d15b1deaefef0b
B00154223 Daniel Animashaun <b00154223@mytudublin.ie>
2025-12-09 21:31:23 +0000

    fixed errors

--- public/confirm.php
+++ public/confirm.php
@@ -1,19 +1,35 @@
 <?php
 session_start();
-
 if (!isset($_POST['confirm'])) {
     header("Location: finalStep.php");
-    exit;
 }
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Signup Confirmed</title>
-</head>
-<body>
-<h1>Your Pilates signup is complete.</h1>
-</body>
-</html>
+$fn = $_SESSION['firstname'];
+$sn = $_SESSION['surname'];
+$em = $_SESSION['email'];
+$ct = $_SESSION['classType'];
+$equipment = $_SESSION['equipment'];
+
+if ($equipment == '1') {
+    $m = $_SESSION['mats'];
+    $b = $_SESSION['blocks'];
+} else {
+    $m = 0;
+    $b = 0;
+}
+
+include("dbcon.php");
+
+$sql = "insert into signups(firstname,surname,email,classType,equipmentNeeded,mats,blocks) values ";
+$sql .= "('$fn','$sn','$em','$ct','$equipment','$m','$b')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Your Pilates signup was created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+session_destroy();
+?>
